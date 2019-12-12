b46653bcd4aa00d4527425d5800f7f6c8d40bbe5
wissteb <wissteb@gmail.com>
2019-12-12 18:15:37 +0100

    diseño de alta y loggin

--- forms/formAlta.php
+++ forms/formAlta.php
@@ -1,36 +1,47 @@
 <?php echo '<link rel="stylesheet" type="text/css" href="css/main.css">'; ?>
 
 <form class="borde" action="" name="inserta" method="POST" enctype="multipart/form-data">
-	<p>
-		Nombre y apellidos (sólo letras 100 caracteres. Requerido): 		
-		<input name="nombre" type="text">
-	</p>
-	<p>
-		Usuario (letras y números entre 7 y 15 caracteres sin ñ ni tildes. Requerido): 		
+	<div>
+		<strong>Nombre y apellidos<strong> <br>
+		<p class="sub"> (sólo letras 100 caracteres. Requerido): </p>		
+		<input name="nombre" type="text" class="nombre">
+	</div>
+	<div>
+	<strong>Usuario<strong> <br>
+	<p class="sub">(letras y números entre 7 y 15 caracteres sin ñ ni tildes. Requerido): 		</p>
 		<input NAME="usuario" type="text">
-	</p>
+	</div>
 
-	<p>
-		Password (Entre 8 y 15 caracteres. Carácteres permitidos letras, números y _*\/$&#. No se permiten ñ ni tildes. Requerido): 		
+	<div>
+	<strong>Contraseña<strong> <br> 
+	<p class="sub">(Entre 8 y 15 caracteres. Carácteres permitidos letras, números y _*\/$&#. No se permiten ñ ni tildes. Requerido): </p>		
 		<input NAME="pass" type="password">
-	</p>
-	<p>
-		Fecha Nacimiento (formato dd-mm-aaaa. No requerido): 		
+	</div>
+	<div>
+	<strong>Fecha Nacimiento<strong> <br>
+	<p class="sub">(formato dd-mm-aaaa. No requerido):<p> 		
 		<input NAME="fecha" type="text">
-	</p>
-	<p>
-		<label>Foto de perfil (jpg y gif Requerido)Tamaño el que marque el servidor:</label> 
+	</div>
+	<div>
+		<label><strong>Foto De Perfil<strong> <br>
+		<p class="sub">(jpg y gif Requerido)Tamaño el que marque el servidor:</p>
+
+		</label> 
 		<input type="file" size="44" name="imagen">
-	<p>
-    Tipo de suscripción (Requerido)<br><br>
+</div>
+   <div>
+	<strong>Tipo de suscripción<strong> <br><br>
     <input type="radio" name="nivel" value="1">1 mes 
-    <input type="radio" name="nivel" value="2">2 meses 
-    <p>
+	<input type="radio" name="nivel" value="2">2 meses 
+	</div>
+	<br>
+    <div>
 		<input type="submit" name="insertar" value="REGISTRARSE">
-	</p>
-	<p>
+	</div>
+	<br>
+	<div>
 		<input type="submit" name="login" value="LOGIN">
-	</p>
+	</div>
 	<div id="errores">
 	<?php
 	//Añado este código para que el formulario enseñe errores al cargar la página en caso de que los haya
--- forms/formLogin.php
+++ forms/formLogin.php
@@ -1,19 +1,25 @@
+<?php echo '<link rel="stylesheet" type="text/css" href="css/main.css">'; ?>
+
 <form class="borde" action="" name="inicio" method="POST">
-	<p>
-		Usuario (letras y números entre 7 y 15 caracteres sin ñ ni tildes. Requerido): 		<br>
+	<div>
+	Usuario <p class="sub">	(letras y números entre 7 y 15 caracteres sin ñ ni tildes. Requerido): 		<br></p>
 		<input NAME="usuario" type="text">
-	</p>
-	<p>
-		Password (Entre 8 y 15 caracteres. Carácteres permitidos letras, números y _*\/$&#. No se permiten ñ ni tildes. Requerido): 		<br>
+	</div>
+	<div>
+	Password<p class="sub">
+		 (Entre 8 y 15 caracteres. Carácteres permitidos letras, números y _*\/$&#. No se permiten ñ ni tildes. Requerido): 		<br>	</p>
 		<input NAME="pass" type="password">
-	</p>
-    <p>
-		<input type="submit" name="acceder" value="INICIAR SESIÓN">
-	</p>
-    <p>
+</div>
+<div>
+    <p class="sub">
+		<input type="submit" name="acceder" value="INICIAR SESIÓN">	
+
+</p>
+</div>
+<div>
 		<input type="submit" name="registro" value="REGISTRARSE">
 	</p>
-
+</div>
 	</form>
 	<?php
 	//Añado este código para que el formulario enseñe errores al cargar la página en caso de que los haya
